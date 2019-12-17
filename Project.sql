2e6307923950921c3b611b0280add4e00db6af51
martinnebesar <nebesar.martin@gmail.com>
2019-12-16 20:57:21 -0500

    Project updated

--- Project.sql
+++ Project.sql
@@ -19,14 +19,15 @@ GO
 
 INSERT INTO Student(StudentID, FirstName, LastName, PhoneNo, Email, StreetNo, StreetName, City, Province, PostalCode)
 VALUES	('994IT','Paul','Young','6479178274','paul.youngda@gmail.com','354','Dublin Ave','South Port','Saskatchewan','Y244T7'),
-		('851IT','Ashley','Young','6471985348','ashleyyoungeee@gmail.com','778','Joplin Str','Highbridge','Prince Edward','646466'),
-		('954IT','Peter','Goyle','6476547981','petergoyle@gmail.com','654','Brock Island Cres','Westgate','Ontario','QOIULA');
+		('851HO','Ashley','Young','6471985348','ashleyyoungeee@gmail.com','778','Joplin Str','Highbridge','Prince Edward','646466'),
+		('954TO','Peter','Goyle','6476547981','petergoyle@gmail.com','654','Brock Island Cres','Westgate','Ontario','QOIULA');
 
 INSERT INTO Student(StudentID, FirstName, LastName, PhoneNo, Email, StreetNo, StreetName, City, Province, PostalCode)
 VALUES	('857IT','Laura','Cole','647235986','lauracole@gmail.com','3645','Bayview Ave','Markham','Ontario','789465');
 
-DELETE FROM Student WHERE StudentId = '' -- for example '857IT'
+DELETE FROM Student WHERE StudentId = '994IT' -- for example '851HO'
 
+DROP VIEW if EXISTS StudentByLastName 
 CREATE VIEW StudentByLastName
 AS
 	SELECT StudentID, FirstName, LastName, PhoneNo,
@@ -41,35 +42,42 @@ DROP TABLE IF Exists Grades
 CREATE TABLE Grades
 (
 	CourseID		VARCHAR(20)		NOT NULL	CONSTRAINT CourseID PRIMARY KEY,
-	Attendance		DECIMAL(3,2)	NULL,
-	Quizes			DECIMAL(3,2)	NULL,
-	Project			DECIMAL(3,2)	NULL,
-	FinalExam		DECIMAL(3,2)	NULL
+	Attendance		DECIMAL(5,2)	NULL,
+	Quizes			DECIMAL(5,2)	NULL,
+	Project			DECIMAL(5,2)	NULL,
+	FinalExam		DECIMAL(5,2)	NULL
 );
 GO
 
 INSERT INTO Grades(CourseID, Attendance, Quizes, Project, FinalExam)
-VALUES ('', , , , )
+VALUES	('689CR', 92, 85, 78, 83),
+		('397CX', 89, 91, 88, 86),
+		('543NC', 91, 86, 84, 91);
 
-SELECT * FROM Grades
+INSERT INTO Grades(CourseID, Attendance, Quizes, Project, FinalExam)
+VALUES	('689IN', 100, 94, 96, 97);
+
+-- SELECT * FROM Grades
 
 DROP TABLE IF Exists Finance
 CREATE TABLE Finance
 (
 	PaymentID		VARCHAR(5)		NOT NULL	CONSTRAINT PaymentType PRIMARY KEY,
-	PaymentType		VARCHAR(15)		NULL,	
-	Tuition			DECIMAL(5,2)	NULL,
-	LateFees		DECIMAL(5,2)	NULL,
-	Sales			DECIMAL(4,2)	NULL,
-	Scholarship		DECIMAL(5,2)	NULL
+	PaymentType		VARCHAR(15)		NULL,		CONSTRAINT ValidPaymentType CHECK(LEN(PaymentType) > 3),
+	Tuition			DECIMAL(7,2)	NULL,		CONSTRAINT ValidTuition CHECK(Tuition > 0),
+	LateFees		DECIMAL(7,2)	NULL,		CONSTRAINT ValidLateFees CHECK(LateFees > 0),
+	Sales			DECIMAL(7,2)	NULL,		CONSTRAINT ValidSales CHECK(Sales > 0),
+	Scholarship		DECIMAL(7,2)	NULL		CONSTRAINT ValidScholarship CHECK(Scholarship > 0),
 );
 GO
 
 INSERT INTO Finance(PaymentID, PaymentType, Tuition, LateFees, Sales, Scholarship)
-VALUES	('WT001','Wire Transfer', 6.000, 0, 0, 0),
-		('CS001','Cash', 10.000, 0, 1.000, 0),
-		('CS002','Cash', 11.500, 500, 0, 5.000);
-		
+VALUES	('WT001','Wire Transfer', 6000, 0, 0, 0),
+		('CS001','Cash', 10000, 0, 1000, 0),
+		('CS002','Cash', 11500, 500, 0, 5000);
+				
+INSERT INTO Finance(PaymentID, PaymentType, Tuition, LateFees, Sales, Scholarship)
+VALUES	('WT002', 'Wire Transfer', 10000, 0, 1000, 0);
 GO
 
 DELETE FROM Finance WHERE PaymentID = 'CS002' -- for example 'Cash'
@@ -89,18 +97,24 @@ CREATE TABLE Course
 (
 	Course2ID		VARCHAR(20)		NOT NULL	CONSTRAINT Course2ID	PRIMARY KEY,
 	CourseName		VARCHAR(50)		NULL,
-	Price			DECIMAL(5,2)	NULL,
+	Price			DECIMAL(7,2)	NULL,
 	Length			TINYINT			NULL,
 	Coop			BIT				NULL
 );
 GO
 
 INSERT INTO Course(Course2ID, CourseName, Price, Length, Coop)
-VALUES	('689CR', 'Microsoft .NET Developer', 6.000, 20, 1),
-		('397CX', 'Network Administrator', 10.000, 26, 1),
-		('543NC', 'Travel and Tourism', 11.500, 30, 0);
-
+VALUES	('689CR', 'Microsoft .NET Developer', 6000, 20, 1),
+		('397CX', 'Hospitality', 10000, 26, 1),
+		('543NC', 'Travel and Tourism', 11500, 30, 0);
 GO
+
+INSERT INTO Course(Course2ID, CourseName, Price, Length, Coop)
+VALUES	('689IN', 'Network Administrator', 10000, 20, 1);
+GO
+;
+
+DELETE FROM Course WHERE CourseID = '' -- for example '397CX'
 
 CREATE VIEW CourseByCourseName
 AS
@@ -125,7 +139,11 @@ CREATE TABLE School
 	PostalCode		VARCHAR(6)		NULL
 );
 GO
-SELECT * FROM School
+DROP TABLE IF Exists School
+INSERT INTO School(SchoolName, PhoneNo, Dli, StreetNo, StreetName, City, Province, PostalCode)
+VALUES ('IBT', '6478049899', 'O469857228347', '247', 'John Street', 'Sarnia', 'ON', 'LWA455');
+
+-- SELECT * FROM School
 
 DROP TABLE IF Exists Instructor
 CREATE TABLE Instructor
